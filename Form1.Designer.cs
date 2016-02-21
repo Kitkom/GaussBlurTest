2c118c88c2ff9ecc22810cdca832ad3a1b585782
Incdir <Kitkom.xu@gmail.com>
2016-02-21 20:03:36 +0800

    for fun :)

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace GaussBlurTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sigmaTxtBox = new System.Windows.Forms.TextBox();
+            this.rangeTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(519, 12);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(35, 12);
+            this.Label1.TabIndex = 1;
+            this.Label1.Text = "Sigma";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(519, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Range";
+            // 
+            // sigmaTxtBox
+            // 
+            this.sigmaTxtBox.Location = new System.Drawing.Point(560, 9);
+            this.sigmaTxtBox.Name = "sigmaTxtBox";
+            this.sigmaTxtBox.Size = new System.Drawing.Size(100, 21);
+            this.sigmaTxtBox.TabIndex = 3;
+            this.sigmaTxtBox.Text = "2";
+            // 
+            // rangeTextBox
+            // 
+            this.rangeTextBox.Location = new System.Drawing.Point(560, 40);
+            this.rangeTextBox.Name = "rangeTextBox";
+            this.rangeTextBox.Size = new System.Drawing.Size(100, 21);
+            this.rangeTextBox.TabIndex = 4;
+            this.rangeTextBox.Text = "2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(521, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 48);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(521, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 48);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Blur";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 528);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rangeTextBox);
+            this.Controls.Add(this.sigmaTxtBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox sigmaTxtBox;
+        private System.Windows.Forms.TextBox rangeTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GaussBlurTest
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap picture;
+
+        public Form1()
+        {
+            InitializeComponent();
+            picture = null;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (picture == null)
+                return;
+
+            Double sigma = Convert.ToDouble(sigmaTxtBox.Text);
+            Int32 range = Convert.ToInt32(rangeTextBox.Text) + 1;
+
+            //core algorithm
+            //weight
+            Double[,] weight = new Double[range, range];
+            Double sum = 0;
+            for (Int32 col = 0; col < range; ++col)
+                for (Int32 row = 0; row < range; ++row)
+                {
+                    weight[col, row] = Math.Exp(-((Double)(col * col + row * row)/ 2 / sigma / sigma));
+                    sum += weight[col, row];
+                    if (col + row == 0)
+                        continue;
+                    if (col * row == 0)
+                        sum += weight[col, row];
+                    else
+                        sum += 3 * weight[col, row];
+                }
+            for (Int32 col = 0; col < range; ++col)
+                for (Int32 row = 0; row < range; ++row)
+                    weight[col, row] /= sum;
+
+            //pixel
+            Int32 width = picture.Width;
+            Int32 height = picture.Height;
+            Bitmap newPicture = new Bitmap(width, height);
+            for (Int32 col = 0; col < width; ++col)
+                for (Int32 row = 0; row < height; ++row)
+                {
+                    Double alpha = 0;
+                    Double red = 0;
+                    Double green = 0;
+                    Double blue = 0;
+                    for (Int32 x = - range + 1; x < range; ++x)
+                        for (Int32 y = - range + 1; y < range; ++y)
+                        {
+                            Double w = weight[Math.Abs(x), Math.Abs(y)];
+                            if ((col + x >= width) || (col + x < 0) || (row + y >= height) || (row + y < 0))
+                                continue;
+                            Color loader = picture.GetPixel(col + x, row + y);
+                            alpha += w * loader.A;
+                            red += w * loader.R;
+                            green += w * loader.G;
+                            blue += w * loader.B;
+                        }
+                    newPicture.SetPixel(col, row, Color.FromArgb((int)alpha, (int)red, (int)green, (int)blue));
+                }
+
+            picture = newPicture;
+            pictureBox1.Image = newPicture;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            picture = (Bitmap)Image.FromFile(openFileDialog1.FileName);
+            pictureBox1.Image = picture;
+        }
+    }
+}
